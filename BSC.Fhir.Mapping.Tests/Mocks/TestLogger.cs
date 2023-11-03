167e42335483dfa872424b9c5ffc985a4d1a2cf0
Jaco Minnaar <jminnaar@bscglobal.com>
2023-11-03 11:43:37 +0200

    update: add logging abstraction that doesn't rely on dependency injection

---
+++ BSC.Fhir.Mapping.Tests/Mocks/TestLogger.cs
@@ -0,0 +1,44 @@
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace BSC.Fhir.Mapping.Tests.Mocks;
+
+public class TestLogger : ILogger
+{
+    public class Scope : IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    private readonly ITestOutputHelper _output;
+
+    public TestLogger(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    public IDisposable? BeginScope<TState>(TState state)
+        where TState : notnull
+    {
+        return new Scope();
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return true;
+    }
+
+    public void Log<TState>(
+        LogLevel logLevel,
+        EventId eventId,
+        TState state,
+        Exception? exception,
+        Func<TState, Exception?, string> formatter
+    )
+    {
+        _output.WriteLine("{0}: {1}", logLevel.ToString(), formatter(state, exception));
+    }
+}
