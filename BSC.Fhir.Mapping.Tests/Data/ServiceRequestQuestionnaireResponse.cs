3a2d3fbc1504ae51b86d4610706ec201fed7b386
Emma <egibson@bscglobal.com>
2023-11-03 09:13:15 +0000

    tidy up

--- BSC.Fhir.Mapping.Tests/Data/ServiceRequestQuestionnaireResponse.cs
+++ BSC.Fhir.Mapping.Tests/Data/ServiceRequestQuestionnaireResponse.cs
@@ -11,16 +11,9 @@ public partial class ServiceRequest
             Status = QuestionnaireResponse.QuestionnaireResponseStatus.Completed,
             Item =
             {
-                /*new()
-                {
-                    LinkId = "patient.id",
-                    Definition = "Patient.id",
-                    Answer = { new QuestionnaireResponse.AnswerComponent { Value = new FhirString(patientId) } }
-                },*/
                 new()
                 {
                     LinkId = "extension",
-                    //Answer = { new QuestionnaireResponse.AnswerComponent { Value = new FhirString("extensionText") } },
                     Item =
                     {
                         new()
@@ -70,30 +63,6 @@ public partial class ServiceRequest
                         }
                     }
                 },
-                /*  new()
-                  {
-                      LinkId = "servicerequest.id",
-                      Definition = "ServiceRequest.id",
-                      Answer = { new QuestionnaireResponse.AnswerComponent { Value = new FhirString(servreqId) } }
-                  },
-                  new()
-                  {
-                      LinkId = "servicerequest.occurrence",
-                      Item =
-                      {
-                          new()
-                          {
-                              LinkId = "servicerequest.occurrence.start",
-                              Answer =
-                              {
-                                  new QuestionnaireResponse.AnswerComponent
-                                  {
-                                      Value = new FhirDateTime("2021-01-01T00:00:00Z")
-                                  }
-                              }
-                          }
-                      }
-                  }*/
             }
         };
 
