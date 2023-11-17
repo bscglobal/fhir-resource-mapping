d1323da5491e9ae216fdc121238011ffd68d0ac8
Jaco Minnaar <jminnaar@bscglobal.com>
2023-11-17 15:16:45 +0200

    refactor: remove IDependecyResolver

--- BSC.Fhir.Mapping/Core/IDependencyResolver.cs
+++
@@ -1,8 +0,0 @@
-using BSC.Fhir.Mapping.Expressions;
-
-namespace BSC.Fhir.Mapping.Core.Expressions;
-
-public interface IDependencyResolver
-{
-    Task<Scope?> ParseQuestionnaireAsync(CancellationToken cancellationToken = default);
-}
