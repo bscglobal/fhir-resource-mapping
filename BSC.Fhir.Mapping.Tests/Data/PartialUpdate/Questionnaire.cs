17b911d4e707f601f0b4d9ca150c4fb873a984dd
Jaco Minnaar <jminnaar@bscglobal.com>
2023-12-01 13:06:22 +0200

    test: add test case for updating part of a group

---
+++ BSC.Fhir.Mapping.Tests/Data/PartialUpdate/Questionnaire.cs
@@ -0,0 +1,73 @@
+using BSC.Fhir.Mapping.Expressions;
+using Hl7.Fhir.Model;
+
+namespace BSC.Fhir.Mapping.Tests.Data;
+
+public partial class PartialUpdate
+{
+    public static Questionnaire CreateQuestionnaire()
+    {
+        return new()
+        {
+            Id = Guid.NewGuid().ToString(),
+            Name = "PartialUpdateQuestionnaire",
+            Title = "Partial Update Questionnaire",
+            SubjectType = new[] { ResourceType.Patient as ResourceType? },
+            Status = PublicationStatus.Draft,
+            Extension =
+            {
+                new()
+                {
+                    Url = Constants.LAUNCH_CONTEXT,
+                    Extension =
+                    {
+                        new()
+                        {
+                            Url = "name",
+                            Value = new Coding
+                            {
+                                System = "http://hl7.org/fhir/uv/sdc/CodeSystem/launchContext",
+                                Code = "patient",
+                                Display = "Patient"
+                            }
+                        },
+                        new() { Url = "type", Value = new FhirString("Patient") }
+                    }
+                },
+                new()
+                {
+                    Url = Constants.EXTRACTION_CONTEXT,
+                    Value = new Expression
+                    {
+                        Language = Constants.FHIR_QUERY_MIME,
+                        Expression_ = "Patient?_id={{%patient.id}}"
+                    }
+                }
+            },
+            Item =
+            {
+                new()
+                {
+                    LinkId = "name",
+                    Definition = "Patient.name",
+                    Type = Questionnaire.QuestionnaireItemType.Group,
+                    Item =
+                    {
+                        new()
+                        {
+                            LinkId = "givenName",
+                            Definition = "Patient.name.given",
+                            Type = Questionnaire.QuestionnaireItemType.String,
+                        },
+                        new()
+                        {
+                            LinkId = "familyName",
+                            Definition = "Patient.name.family",
+                            Type = Questionnaire.QuestionnaireItemType.String,
+                        },
+                    }
+                },
+            }
+        };
+    }
+}
---
+++ BSC.Fhir.Mapping.Tests/Data/PartialUpdate/QuestionnaireResponse.cs
@@ -0,0 +1,45 @@
+using Hl7.Fhir.Model;
+
+namespace BSC.Fhir.Mapping.Tests.Data;
+
+public partial class PartialUpdate
+{
+    public static QuestionnaireResponse CreateQuestionnaireResponse()
+    {
+        return new()
+        {
+            Status = QuestionnaireResponse.QuestionnaireResponseStatus.Completed,
+            Item =
+            {
+                new()
+                {
+                    LinkId = "name",
+                    Item =
+                    {
+                        new() { LinkId = "givenName" },
+                        new() { LinkId = "familyName", Answer = { new() { Value = new FhirString("Smith") } } }
+                    }
+                }
+            }
+        };
+    }
+
+    public static QuestionnaireResponse CreateQuestionnaireResponseWithoutGiven()
+    {
+        return new()
+        {
+            Status = QuestionnaireResponse.QuestionnaireResponseStatus.Completed,
+            Item =
+            {
+                new()
+                {
+                    LinkId = "name",
+                    Item =
+                    {
+                        new() { LinkId = "familyName", Answer = { new() { Value = new FhirString("Smith") } } }
+                    }
+                }
+            }
+        };
+    }
+}
