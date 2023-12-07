d3fa6d66858fbcc042bd005e68df83dcb2f98c9b
Jaco Minnaar <jminnaar@bscglobal.com>
2023-12-07 09:58:17 +0200

    test: add test case for general note with no images

--- BSC.Fhir.Mapping.Tests/Data/GeneralNote/GeneralNoteQuestionnaireResponse.cs
+++ BSC.Fhir.Mapping.Tests/Data/GeneralNote/GeneralNoteQuestionnaireResponse.cs
@@ -1,10 +1,9 @@
 using System.Text;
 using Hl7.Fhir.Model;
-using Hl7.Fhir.Serialization;
 
 namespace BSC.Fhir.Mapping.Tests.Data;
 
-public partial class GeneralNote
+public static partial class GeneralNote
 {
     public static QuestionnaireResponse CreateQuestionnaireResponse(
         string compositionId,
@@ -12,73 +11,47 @@ public partial class GeneralNote
         IReadOnlyCollection<string> imageIds
     )
     {
-        QuestionnaireResponse response =
-            new()
+        var response = BaseResponse(compositionId).AddNote(noteId).AddImages(imageIds);
+
+        return response;
+    }
+
+    private static QuestionnaireResponse AddNote(this QuestionnaireResponse response, string noteId)
+    {
+        response.Item.Add(
+            new QuestionnaireResponse.ItemComponent
             {
+                LinkId = "note",
                 Item =
                 {
-                    new() { LinkId = "composition.id", Answer = { new() { Value = new FhirString(compositionId) } } },
-                    new() { LinkId = "composition.date", },
+                    new() { LinkId = "note.id", Answer = { new() { Value = new FhirString(noteId) } } },
                     new()
                     {
-                        LinkId = "composition.event",
+                        LinkId = "note.content",
                         Item =
                         {
                             new()
                             {
-                                LinkId = "composition.event.period",
-                                Item =
+                                LinkId = "note.content.attachment",
+                                Answer =
                                 {
-                                    new()
-                                    {
-                                        LinkId = "composition.event.period.start",
-                                        Answer = { new() { Value = new FhirDateTime("2023-10-29") } }
-                                    }
+                                    new() { Value = new Attachment { Data = Encoding.UTF8.GetBytes("Hello World") } }
                                 }
                             }
                         }
-                    },
-                    new()
-                    {
-                        LinkId = "procedureCode",
-                        Answer =
-                        {
-                            new()
-                            {
-                                Value = new Coding { System = "ProcedureCode", Code = "DEF002" }
-                            }
-                        }
-                    },
-                    new() { LinkId = "composition.extension" },
-                    new()
-                    {
-                        LinkId = "note",
-                        Item =
-                        {
-                            new() { LinkId = "note.id", Answer = { new() { Value = new FhirString(noteId) } } },
-                            new()
-                            {
-                                LinkId = "note.content",
-                                Item =
-                                {
-                                    new()
-                                    {
-                                        LinkId = "note.content.attachment",
-                                        Answer =
-                                        {
-                                            new()
-                                            {
-                                                Value = new Attachment { Data = Encoding.UTF8.GetBytes("Hello World") }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    },
+                    }
                 }
-            };
+            }
+        );
 
+        return response;
+    }
+
+    private static QuestionnaireResponse AddImages(
+        this QuestionnaireResponse response,
+        IReadOnlyCollection<string> imageIds
+    )
+    {
         response.Item.AddRange(
             imageIds.Select(
                 id =>
@@ -95,4 +68,45 @@ public partial class GeneralNote
 
         return response;
     }
+
+    private static QuestionnaireResponse BaseResponse(string compositionId) =>
+        new()
+        {
+            Item =
+            {
+                new() { LinkId = "composition.id", Answer = { new() { Value = new FhirString(compositionId) } } },
+                new() { LinkId = "composition.date", },
+                new()
+                {
+                    LinkId = "composition.event",
+                    Item =
+                    {
+                        new()
+                        {
+                            LinkId = "composition.event.period",
+                            Item =
+                            {
+                                new()
+                                {
+                                    LinkId = "composition.event.period.start",
+                                    Answer = { new() { Value = new FhirDateTime("2023-10-29") } }
+                                }
+                            }
+                        }
+                    }
+                },
+                new()
+                {
+                    LinkId = "procedureCode",
+                    Answer =
+                    {
+                        new()
+                        {
+                            Value = new Coding { System = "ProcedureCode", Code = "DEF002" }
+                        }
+                    }
+                },
+                new() { LinkId = "composition.extension" },
+            }
+        };
 }
